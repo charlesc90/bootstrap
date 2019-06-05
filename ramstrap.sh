2cf39383678f5b38fa922d4f0ebd2fee806fc4c2
Charles Cox <charles.h.cox@gmail.com>
2019-06-05 08:48:09 -0500

    Update ramstrap.sh

--- ramstrap.sh
+++ ramstrap.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
+## setup #####################################################################
+
+# URI of a debian mirror
+dmir="http://ftp.us.debian.org/debian/"
+
+# check for a zram directory in your home directory
 if [ -d $HOME/zram ]; then
     export ZHOME=$HOME/zram
+# if there isn't one, make one
 else
     sudo mkdir -v $HOME/zram && export ZHOME=$HOME/zram
 fi
 
-sudo modprobe zram
-sudo echo "$(zramctl -f)" > zram.tmp
-sudo cat zram.tmp | echo | > zram0.tmp
-sudo zramctl -s 1024M -t 8 "$(sudo cat zram0.tmp)"
-sudo blkid --probe "$(sudo cat zram0.tmp)"
-sudo mkfs.ext4 -v "$(sudo cat zram0.tmp)"
-sudo mount "$(sudo cat zram0.tmp)" -v -t ext4 $HOME/zram
+###############################################################################
 
-sudo debootstrap --arch amd64                                \
-    --include=build-essential,nano,bash-completion           \
-    sid $HOME/zram http://ftp.us.debian.org/debian/ 
+# First, probe the zram module. It is unlikely to be in your kernel by default.
+# If it's already in there, modprobing it will not cause any problems.
+sudo modprobe zram
+
+# This line doesn't need to be this complicated. It writes the name of the
+# available zram block device to temp, then zramctl initializes it using temp
+# (dependent on a zram block device being available)
+echo "$(zramctl -f)" > temp && zramctl -s 2048M -t 8 "$(cat temp)"
+
+export ZBLOCK="$(cat temp)"
+diff "$(cat temp)" "$(echo $ZBLOCK)"
+
+blkid --probe $ZBLOCK
+mkfs.ext4 -v $ZBLOCK
+mount $ZBLOCK -v -t ext4 $ZHOME
+
+debootstrap --arch amd64 --include=build-essential sid $ZHOME ${dmir} 
